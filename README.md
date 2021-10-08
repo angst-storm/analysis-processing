991fc0eaca17f3911f3c61669229d92ddd0c3488
angst-storm <Sergey.Kiprin@urfu.me>
2021-10-09 00:43:00 +0500

    первый интерфейс парсера

--- README.md
+++
@@ -1,1 +0,0 @@
-# analysis-processing
---
+++ SillyParser.py
@@ -0,0 +1,7 @@
+from TableParser import TableParser
+
+
+# вспомогательный тестовый парсер-заглушка для разработчика REST
+class SillyParser(TableParser):
+    def parse_pdf(self, input_file):
+        pass
