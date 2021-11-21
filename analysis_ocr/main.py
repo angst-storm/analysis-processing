770007e32aade8e7557ed1a9af88169312946ef3
sasharumega <92515117+sasharumega@users.noreply.github.com>
2021-11-21 15:17:13 +0500

    Небольшой рефакторинг + Исправление текста
    Добавлено исправление текста, основанное на расстоянии Левенштейна
    Заменяются слова, которые похожи на слова из листа заготовленных слов
    (лист со словами находится в методе "get_optimized_string")

--- analysis_ocr/main.py
+++ analysis_ocr/main.py
@@ -3,53 +3,110 @@ import shutil
 import sys
 from ocr import convert_image_to_csv
 import pdf2jpg
+import Levenshtein as lev
 
-def log(s):
-	# В будущем возможно добавим логи
-	print(s)
-
-ispdf = False
-show_output = False
-
+# Вывод логов
+show_advanced_output = False
 # Эта штука ответственная за удаление цели после обработки
 delete_file = False
 
+
+def log(s):
+	# В будущем возможно добавим логи
+	if show_advanced_output:
+		print(s)
+
+
+def execute_preparing_stage(filepath):
+	global ispdf
+	if filepath[len(filepath) - 3:] == 'jpg':
+		log('Обнаружен JPG')
+
+	elif filepath[len(filepath) - 3:] == 'pdf':
+		ispdf = True
+		log('Обнаружен PDF')
+		log('\nКонвертация в JPG...')
+		pdf2jpg.convert_pdf2jpg(filepath, filepath[:len(filepath) - 4], dpi=300, pages="0")
+		filepath = filepath[:len(filepath) - 4] + '\\' + filepath[:len(filepath) - 4] + '.pdf_dir\\0_' + filepath[:len(
+			filepath) - 4] + '.pdf.jpg'
+		log('Готово')
+	return filepath
+
+
+def execute_text_recognition_stage():
+	text = convert_image_to_csv(filepath)
+	if show_advanced_output:
+		log("\n---------------------------\n")
+		log(text)
+		log("\n---------------------------\n")
+	return text
+
+
+def execute_optimize_stage():
+	optimized_csv_output_str = ''
+	lines = csv_output_str.replace('\r', '').lower().split('\n')
+	for line in lines:
+		str_list = line.split(',')
+		new_line_list = list(map(lambda x: get_optimized_string(x), str_list))
+
+		if show_advanced_output:
+			print(new_line_list)
+
+		optimized_csv_output_str += '\t'.join(new_line_list)
+		optimized_csv_output_str += '\r\n'
+	return optimized_csv_output_str
+
+
+def execute_csv_write_stage(optimized_csv_output_str):
+	f = open('done.csv', 'w')
+	f.write(optimized_csv_output_str)
+	f.close()
+
+
+def execute_cleaning_stage():
+	if delete_file:
+		os.remove(original_filepath)
+	shutil.rmtree(f'{filepath[:len(filepath) - 4]}')
+	if ispdf:
+		shutil.rmtree(f'{original_filepath[:len(original_filepath) - 4]}')
+
+
+def get_optimized_string(s):
+	# Высчитывает расстояние Левенштейна со списком слов
+	# и выбирает максимально подходящее (если такое есть)
+	words = ['лейкоциты', 'лимфоциты', 'моноциты',
+			 'нейтрофилы', 'эозинофилы', 'базофилы',
+			 'эритроциты','гемоглобин','гематокрит',
+			 'тромбоциты']
+	min_distance = 1000
+	min_distance_word = s
+	for word in words:
+		distance = lev.distance(s, word)
+		# Если из одной строки к другой можно прийти меньше, чем за
+		# 5 изменений, то это претендет на замену
+		if distance < 5 and distance < min_distance:
+			min_distance = distance
+			min_distance_word = word
+	return min_distance_word
+
+
 log('Запуск...')
 
+ispdf = False
 filepath = input()
 original_filepath = filepath
 
-if filepath[len(filepath)-3:] == 'jpg':
-	log('Обнаружен JPG')
+log('\nОбработка файла...')
+filepath = execute_preparing_stage(filepath)
 
-elif filepath[len(filepath)-3:] == 'pdf':
-	ispdf = True
-	log('Обнаружен PDF')
-	log('\nКонвертация в JPG...')
-	pdf2jpg.convert_pdf2jpg('..\\'+filepath, '..\\'+filepath[:len(filepath)-4], dpi=300, pages="0")
-	filepath = filepath[:len(filepath)-4] + '\\' + filepath[:len(filepath)-4] + '.pdf_dir\\0_' + filepath[:len(filepath)-4] + '.pdf.jpg'
-	log('Готово')
-
-log('Путь: '+filepath)
 log('\nЗапуск распознавающего модуля...')
-csv_output = convert_image_to_csv(filepath)
-log('Готово')
+csv_output_str = execute_text_recognition_stage()
 
-if show_output:
-	print("\n---------------------------\n")
-	print(csv_output)
-	print("\n---------------------------\n")
+log('\nОптимизация текста...')
+optimized_csv_output_str = execute_optimize_stage()
 
 log('\nЗапись CSV файла...')
-f = open('done.csv', 'w')
-f.write(csv_output)
-f.close()
-log('Готово')
+execute_csv_write_stage(optimized_csv_output_str)
 
 log('\nОчистка мусора...')
-if delete_file:
-	os.remove(original_filepath)
-shutil.rmtree(f'{filepath[:len(filepath) - 4]}')
-if ispdf:
-	shutil.rmtree(f'{original_filepath[:len(original_filepath) - 4]}')
-print('Готово')
+execute_cleaning_stage()
