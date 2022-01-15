383b246886ccfcaf9af0f2b13ad9df428feb442d
AshNRush <72685173+AshNRush@users.noreply.github.com>
2022-01-15 14:49:20 +0500

    изменил return

--- restapi/parsers/tabula_parser.py
+++ restapi/parsers/tabula_parser.py
@@ -11,11 +11,13 @@ def get_table_from_images(image_path, lab):
         stream = False
         lattice = True
     pdf_dfs = tabula.read_pdf(image_path, pages="all", multiple_tables=True, stream=stream, lattice=lattice, guess=True)
+    csv = ''
 
     #TODO: сделать валидацию пустых строк (если 2+ поля в строке пусты, слить эту строку со строкой выше)
     # и проверку содержимого DataFrame'а (проверить 2-3 строки на наличие слов-индикаторов, если такие слова имеются, добавить текущий датафрейм к финальному)
 
-    return pdf_dfs[0].to_csv
+    for df in pdf_dfs:
+        csv += df.to_csv
 
     #Цикл для проверки содержимого датафреймов
     #for i in range(len(pdf_dfs)):
